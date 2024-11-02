760d6ee840fada3d082f2dfda4447e3a557844b3
Vinder1 <kar.galiullin@gmail.com>
2024-11-02 18:24:07 +0300

    picture drawn

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("=============");
+        Console.WriteLine("=--@-----@--=");
+        Console.WriteLine("=-----@-----=");
+        Console.WriteLine("=-@-------@-=");
+        Console.WriteLine("=--@@@@@@@--=");
+        Console.WriteLine("=============");
+        Console.ReadLine();
     }
 }
