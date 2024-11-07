922b0d8a2f15c886b197f8d3df42ce5d6e60619f
Vinder1 <kar.galiullin@gmail.com>
2024-11-07 09:56:11 +0300

    add form

--- TypeWorkshop/Program.cs
+++ TypeWorkshop/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Введите: имя, возраст, курс, группа, есть ли питомец (True/False)");
+        string name = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+        int course = int.Parse(Console.ReadLine());
+        string group = Console.ReadLine();
+        bool hasPet = bool.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ваша анкета:");
+        Console.WriteLine("Имя          | " + name);
+        Console.WriteLine("Возраст      | " + age);
+        Console.WriteLine("Курс         | " + course);
+        Console.WriteLine("Группа       | " + group);
+        Console.WriteLine("Есть питомец | " + hasPet);
     }
 }
